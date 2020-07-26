89ed424c527400b62d9197ae79ef680a3199553e
Bhargav Naik <bhargavnaik07@gmail.com>
2020-07-27 02:18:03 +0530

    backend: Park create get_by_id get_batch

--- backend/app/resources/park.py
+++ backend/app/resources/park.py
@@ -1,27 +1,22 @@
 from flask import request
 from flask_restful import Resource, reqparse
-from dal.models import Park
+from dal.models import Park as ParkModel
 from dal.schemas import ParkSchema
 from resources.errors import InternalServerError
 
+park_schema = ParkSchema()
+parks_schema = ParkSchema(many=True)
+
 
 class Park(Resource):
+
     def get(self, id):
-        return {
-            "name" : "Disney World",
-            "image_id" : "image123",
-            "description" : "Long text",
-            "user_id" : "user@gmail.com",
-            "location" : {
-                "lat" : 23.0078201,
-                "lng" : 88.5428696
-            }
-        }, 200
+        park = ParkModel.objects.get_or_404(id=id)
+        return park_schema.dump(park).data, 200
 
 
 class Parks(Resource):
     def __init__(self):
-        self.park_schema = ParkSchema()
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('offset', type=int, required=True)
         self.reqparse.add_argument('limit', type=int, required=True)
@@ -29,20 +24,24 @@ class Parks(Resource):
 
     def get(self):
         args = self.reqparse.parse_args()
-        print(f"offset: {args['offset']}")
-        print(f"limit: {args['limit']}")
-        parks = Park.objects().to_json()
-        return parks, 200
+        offset = args['offset']
+        limit = args['limit']
+        # TODO figure out how to use pagination while querying mongodb
+        parks = ParkModel.objects()
+        parklist = parks_schema.dump(parks).data
+        return {'parks': parklist}, 200
 
     def post(self):
         json_data = request.get_json()
+
         if not json_data:
             return {'message': 'No input data provided'}, 400
-        park, errors = self.park_schema.load(json_data)
+        park, errors = park_schema.load(json_data)
         if errors:
             return errors, 400
 
         try:
             park.save()
+            return park_schema.dump(park).data, 200
         except Exception:
             raise InternalServerError
