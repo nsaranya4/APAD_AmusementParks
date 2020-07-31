9c851f9ec138457814570168323a4e802eb6fb48
Bhargav Naik <bhargavnaik07@gmail.com>
2020-07-31 23:32:52 +0530

    ui: added park and post http clients

--- ui/app/amusepark/clients/post_client.py
+++ ui/app/amusepark/clients/post_client.py
@@ -12,16 +12,20 @@ class PostClient:
     def create(self, create_post_request):
         data = self.create_post_request_schema.dump(create_post_request).data
         response = requests.post(self.post_path, data=data)
-        post = self.post_schema.load(response.json())
+        # TODO:: handle error codes
+        post = self.post_schema.load(response.json()).data
         return post
-
-    def get_by_user_id(self, user_id, offset, limit):
-        params = {'user_id': user_id, offset: offset, limit: limit}
-        response = requests.get(self.post_path, params=params)
-        posts = self.posts_schema.load(response.json())
-        return posts
 
     def get_by_id(self, id):
         response = requests.get(self.post_path + "/" + id)
+        # TODO:: handle error codes
         post = self.post_schema.load(response.json()).data
         return post
+
+    def get_batch(self, filters, offset, limit):
+        pagination = {'offset': offset, 'limit': limit}
+        params = {**filters, **pagination}
+        response = requests.get(self.post_path, params=params)
+        #TODO:: handle error codes
+        posts = self.posts_schema.load(response.json()).data
+        return posts
