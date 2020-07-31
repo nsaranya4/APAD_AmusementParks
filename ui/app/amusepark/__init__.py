ba714d9380361dae553c853f0421e0e1f4d6f0d6
Bhargav Naik <bhargavnaik07@gmail.com>
2020-07-31 18:47:25 +0530

    ui: added client

--- ui/app/amusepark/__init__.py
+++ ui/app/amusepark/__init__.py
@@ -8,6 +8,9 @@ def create_app():
     # Set the secret key to some random bytes
     app.secret_key = os.urandom(16)
 
+    from .routes.auth_routes import auth_crud
+    app.register_blueprint(auth_crud, url_prefix='/')
+
     from .routes.post_routes import post_crud
     app.register_blueprint(post_crud, url_prefix='/posts')
 
