89759753213d8cc34f9d821909ed68213d951a2e
Bhargav Naik <bhargavnaik07@gmail.com>
2020-08-01 23:30:50 +0530

    ui: view my posts

--- ui/app/amusepark/routes/user_routes.py
+++ ui/app/amusepark/routes/user_routes.py
@@ -1,32 +1,26 @@
 from flask import Blueprint, redirect, render_template, request, url_for, session
 from flask import make_response, send_file, jsonify
 from ..clients.post_client import PostClient
+from ..clients.user_client import UserClient
 
 user_crud = Blueprint('user', __name__)
 post_client = PostClient("http://localhost:5000")
-
-
-@user_crud.route('/<manage>')
-def management(manage):
-    user_id = ""
-    if manage == 'posts':
-        return redirect(url_for('.view_posts', id=str(user_id)))
-    elif manage == 'parks':
-        return redirect(url_for('.view_parks', id=str(user_id)))
-    elif manage == 'subscriptions':
-        return redirect(url_for('.view_subscriptions', id=str(user_id)))
-    else:
-        return redirect(url_for('.view_posts', id=str(user_id)))
+user_client = UserClient("http://localhost:5000")
 
 
 @user_crud.route('/<id>/posts')
-def view_posts(id):
-    return
-
-
-@user_crud.route('/<id>/parks')
-def view_parks(id):
-    return
+def view_all_post_of_user(id):
+    page = request.args.get('page', None)
+    if page:
+        page = page.encode('utf-8')
+        skip = int(page)
+    else:
+        skip = 0
+    limit = 10
+    offset = skip * 10
+    user = user_client.get_by_id(id)
+    posts = post_client.get_batch({'user_id': id}, offset, limit)
+    return render_template('myposts.html', posts=posts, user=user)
 
 
 @user_crud.route('/<id>/subscriptions')
