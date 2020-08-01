f7b25b45a6968ece6c3185db44635d75f85ec518
nsaranya4 <nsaranya4@gmail.com>
2020-08-01 11:33:18 -0500

    added subscription service,repo

--- backend/app/repos/subscription_repo.py
+++ backend/app/repos/subscription_repo.py
@@ -10,8 +10,9 @@ class SubscriptionRepo:
         subscription_list = Subscription.objects
         if filters.keys().__contains__('user_id'):
             subscription_list = subscription_list.filter(user=filters['user_id'])
-        if filters.keys().__contains__('park_id'):
-            subscription_list = subscription_list.filter(park=filters['park_id'])
+    
+        #if filters.keys().__contains__('park_id'):
+            #subscription_list = subscription_list.filter(park=filters['park_id'])
         return subscription_list
 
     def create(self, post: Post):
