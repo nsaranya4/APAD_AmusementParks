0455f38cda22d12add6a5be883d04bf7677a97a6
nsaranya4 <nsaranya4@gmail.com>
2020-08-01 10:12:14 -0500

    added subscription repo

---
+++ backend/app/repos/subscription_repo.py
@@ -0,0 +1,19 @@
+from models.subscription import Subscription
+
+
+class SubscriptionRepo:
+
+    def get_by_id(self, id: str):
+        return Subscription.objects.get(id=id)
+
+    def get_batch(self,filters: dict):
+        subscription_list = Subscription.objects
+        if filters.keys().__contains__('user_id'):
+            subscription_list = subscription_list.filter(user=filters['user_id'])
+        if filters.keys().__contains__('park_id'):
+            subscription_list = subscription_list.filter(park=filters['park_id'])
+        return subscription_list
+
+    def create(self, post: Post):
+        Subscription.save()
+        return Subscription
