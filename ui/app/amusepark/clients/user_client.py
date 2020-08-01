84c48af3214edaeea67ae69a9d4c4ff6a3bbd671
Bhargav Naik <bhargavnaik07@gmail.com>
2020-08-02 02:52:33 +0530

    ui: added support for subscriptions

--- ui/app/amusepark/clients/user_client.py
+++ ui/app/amusepark/clients/user_client.py
@@ -1,16 +1,22 @@
 import requests
 from ..representations.user import CreateUserRequestSchema, UserSchema
+from ..representations.subscription import CreateSubscriptionRequestSchema, SubscriptionSchema
 
 
 class UserClient:
     def __init__(self, base_url):
         self.user_path = base_url + '/funtech/v1/users'
+        self.subscription_path = base_url + '/funtech/v1/subscriptions'
         self.create_user_request_schema = CreateUserRequestSchema()
         self.user_schema = UserSchema()
+        self.create_subscription_request_schema = CreateSubscriptionRequestSchema()
+        self.subscription_schema = SubscriptionSchema()
+        self.subscriptions_schema = SubscriptionSchema(many=True)
+        self.headers = {"Content-Type": "application/json", "Accept": "*/*"}
 
     def create(self, create_user_request):
-        data = self.create_user_request_schema.dump(create_user_request).data
-        response = requests.post(self.user_path, data=data)
+        payload = self.create_user_request_schema.dump(create_user_request).data
+        response = requests.post(self.user_path, json=payload, headers=self.headers)
         # TODO:: handle error codes
         user = self.user_schema.load(response.json()).data
         return user
@@ -27,3 +33,24 @@ class UserClient:
         #TODO:: handle error codes
         user = self.user_schema.load(response.json()).data
         return user
+
+    def create_subscription(self, create_subscription_request):
+        data = self.create_subscription_request_schema.dump(create_subscription_request).data
+        response = requests.post(self.subscription_path, json=data, headers=self.headers)
+        # TODO:: handle error codes
+        user = self.subscriptions_schema.load(response.json()).data
+        return user
+
+    def create_subscription(self, create_subscription_request):
+        payload = self.create_subscription_request_schema.dump(create_subscription_request).data
+        response = requests.post(self.subscription_path, json=payload, headers=self.headers)
+        # TODO:: handle error codes
+        subscription = self.subscription_schema.load(response.json()).data
+        return subscription
+
+    def get_subscriptions(self, user_id):
+        params = {'user_id': user_id}
+        response = requests.get(self.subscription_path, params=params)
+        #TODO:: handle error codes
+        subscriptions = self.subscriptions_schema.load(response.json()).data
+        return subscriptions
