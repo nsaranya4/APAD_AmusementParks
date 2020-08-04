fde62d0c20e4da6daa55812bc278673b214bdbc2
Bhargav Naik <bhargavnaik07@gmail.com>
2020-08-05 04:10:04 +0530

    ui: added search box functionality in a ver hacky manner

--- ui/app/amusepark/routes/post_routes.py
+++ ui/app/amusepark/routes/post_routes.py
@@ -24,6 +24,21 @@ def construct_post_blueprint(post_client):
         posts = post_client.get_batch({'tag': tag}, offset, limit)
         return render_template('myposts.html', posts=posts)
 
+    @post_crud.route('/tag', methods=['POST'])
+    def view_posts_with_tag():
+        data = request.form.to_dict(flat=True)
+        tag = data['searchtext']
+        page = request.args.get('page', None)
+        if page:
+            page = page.encode('utf-8')
+            skip = int(page)
+        else:
+            skip = 0
+        limit = 10
+        offset = skip * 10
+        posts = post_client.get_batch({'tag': tag}, offset, limit)
+        return render_template('myposts.html', posts=posts)
+
     @post_crud.route('/create', methods=['POST'])
     def create():
         if request.method == 'POST':
