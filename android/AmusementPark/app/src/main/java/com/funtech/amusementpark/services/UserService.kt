f84c6b9fbe57229cf3b438de1c646eae3e9f0874
Gowtamik <gowtami.khambhampati@utexas.edu>
2020-08-12 17:26:08 -0500

    android: UserService.kt

--- android/AmusementPark/app/src/main/java/com/funtech/amusementpark/services/UserService.kt
+++ android/AmusementPark/app/src/main/java/com/funtech/amusementpark/services/UserService.kt
@@ -1,11 +1,16 @@
 package com.funtech.amusementpark.services
 
+import com.funtech.amusementpark.models.CreateUserRequest
 import com.funtech.amusementpark.models.User
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface UserService {
     @GET("/funtech/v1/users")
     fun getUserByEmail(@Query("email") email: String): Call<User>
+
+    @POST("/funtech/v1/users")
+    @FormUrlEncoded
+    fun createUser(@Body createUserRequest: CreateUserRequest): Call<User>
+
 }
