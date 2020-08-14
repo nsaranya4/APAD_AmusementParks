b5e6e0e57a8828d17479af8d2db16d43ca95a33e
Gowtamik <gowtami.khambhampati@utexas.edu>
2020-08-14 10:28:26 -0500

    android: change in post.kt file

--- android/AmusementPark/app/src/main/java/com/funtech/amusementpark/models/Post.kt
+++ android/AmusementPark/app/src/main/java/com/funtech/amusementpark/models/Post.kt
@@ -1,4 +1,4 @@
 package com.funtech.amusementpark.models
 
-data class Post(val id: String, val title: String, val image_id: String,
+data class Post(val id: String, val title: String, val image_id: String, val tags: List<String>,
                 val description: String, val location: Location, val user: User, val park: Park)
